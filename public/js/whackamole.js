5ca008f852f97a048f97f70eb7e203349a586b69
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-05 15:40:09 -0600

    Added pictures to whackamole and random number is generating

---
+++ public/js/whackamole.js
@@ -0,0 +1,25 @@
+console.log('whackamole js file is linked correctly');
+(function() {
+
+var moleRappers = $('.moleDivs');
+console.log(moleRappers);
+// console.log(moleRappers.length);
+
+// console.log(moleRappers[2]);
+
+var random = Math.floor( Math.random()* 9);
+console.log(random);
+
+
+// $(moleRappers[2]).css('background-image', 'image-2.gif')
+
+
+	// $('#square1').click(function(){
+
+	// 	$(this).fadeOut();
+	// 	console.log('square one faded out');
+	// });
+
+
+
+}) ();
